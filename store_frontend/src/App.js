ef4e0b82691e02e28e42d914b9a09310f217e434
haraphat01 <haraphat@gmail.com>
2022-06-12 12:21:46 +0100

    initialization

---
+++ store_frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="">
+        <a className="App-link" href="/">
+          React
+        </a>
+       
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
