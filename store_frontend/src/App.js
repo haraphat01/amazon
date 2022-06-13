f86f8258dc0b8cfc5b86791c1c75a20086f62946
haraphat01 <haraphat@gmail.com>
2022-06-13 11:48:03 +0100

    update

--- store_frontend/src/App.js
+++ store_frontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -8,9 +8,25 @@ function App() {
         <a className="App-link" href="/">
           React
         </a>
-       
       </header>
       <main>List Products</main>
+      <div>Featured Products</div>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-info">
+            <a href={`/product/${product.slug}`}>
+              <p>{product.name}</p>
+              </a>
+              <p><strong>${product.price}</strong></p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
