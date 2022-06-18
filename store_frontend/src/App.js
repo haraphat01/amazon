f0d6e51b8992cda7d7c9108b5bf437c4a570812d
haraphat01 <haraphat@gmail.com>
2022-06-18 17:27:27 +0100

    ss

--- store_frontend/src/App.js
+++ store_frontend/src/App.js
@@ -2,21 +2,36 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homescreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <header className="">
-          <a className="App-link" href="/">
-            React
-          </a>
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazon</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>List Products</main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<Homescreen />} />
-        </Routes>
+        <Container>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<Homescreen />} />
+          </Routes>
+        </Container>
+        <Container>
+          {' '}
+          <footer>
+            <div>All right reserved</div>
+          </footer>
+        </Container>
       </div>
     </BrowserRouter>
   );
--- store_frontend/src/screens/HomeScreen.js
+++ store_frontend/src/screens/HomeScreen.js
@@ -1,26 +1,65 @@
-import { Link } from "react-router-dom"
-import data from "../../../backend/data"
+import { useEffect, useReducer } from 'react';
+import { Link } from 'react-router-dom';
+// import data from '../../../backend/data';
+import axios from 'axios';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col"
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, error: action.payload, loading: false };
+    default:
+      return state;
+  }
+};
 function Homescreen() {
-    return <div>
-        <div>Featured Products</div>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
+  //   const [products, setProducts] = useState([]);
+  const [{ products, error, loading }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+    products: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: error.message });
+      }
+
+      //   setProducts(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <div>Featured Products</div>
+      <div className="products">
+        {loading ? (
+          <div>loading....</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col sm={6} md={4} lg={3} className="mb-3">
+               <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
     </div>
+  );
 }
-export default Homescreen
+export default Homescreen;
